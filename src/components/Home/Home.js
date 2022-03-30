5b77100b7b3ef1ac0da786b17cbcdc2d5c9124c4
Juhwan <dev.juhwanlee@gmail.com>
2022-03-31 02:31:34 +0900

    Refactoring code

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Home.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const Home = (props) => {
+  const authCtx = useContext(AuthContext);
   return (
     <Card className={classes.home}>
       <h1>어서오세요 반갑습니다 😀</h1>
-      <Button>Logout</Button>
+      <Button onClick={authCtx.onLogout}>Logout</Button>
     </Card>
   );
 };
