3577b7d7e95398a97b5557fbecc682d242b0c996
Juhwan <dev.juhwanlee@gmail.com>
2022-03-30 19:39:58 +0900

    Edit code: some styles, header typo

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -6,8 +6,11 @@ import classes from './MainHeader.module.css';
 const MainHeader = (props) => {
   return (
     <header className={classes['main-header']}>
-      <h1>A Typical Page</h1>
-      <Navigation isLoggedIn={props.isAuthenticated} onLogout={props.onLogout} />
+      <h1>일반적인 페이지</h1>
+      <Navigation
+        isLoggedIn={props.isAuthenticated}
+        onLogout={props.onLogout}
+      />
     </header>
   );
 };
